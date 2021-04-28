5f0627cc4f95ba92787b9d93af7d7de8f620c949
WarrenGSlice <77131351+WarrenGSlice@users.noreply.github.com>
2021-04-28 11:41:56 -0700

    Add files via upload

---
+++ Exercise8/Exercise8/Form1.Designer.cs
@@ -0,0 +1,244 @@
+﻿
+namespace Exercise8
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.FatGramsInput = new System.Windows.Forms.TextBox();
+            this.CarbsInput = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.CaloriesFromFatOutput = new System.Windows.Forms.TextBox();
+            this.CaloriesFromCarbsOutput = new System.Windows.Forms.TextBox();
+            this.TotslCaloriesOutput = new System.Windows.Forms.TextBox();
+            this.CalculateButton = new System.Windows.Forms.Button();
+            this.ExitButton = new System.Windows.Forms.Button();
+            this.ResetButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(222, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(356, 49);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "How Many Calories?";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(19, 114);
+            this.label2.MinimumSize = new System.Drawing.Size(0, 23);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(224, 23);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Enter Number of Fat Grams :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(7, 180);
+            this.label3.MinimumSize = new System.Drawing.Size(0, 23);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(236, 23);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Enter Number of Carb Grams :";
+            // 
+            // FatGramsInput
+            // 
+            this.FatGramsInput.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.FatGramsInput.Location = new System.Drawing.Point(249, 114);
+            this.FatGramsInput.Name = "FatGramsInput";
+            this.FatGramsInput.Size = new System.Drawing.Size(100, 23);
+            this.FatGramsInput.TabIndex = 3;
+            // 
+            // CarbsInput
+            // 
+            this.CarbsInput.Location = new System.Drawing.Point(249, 180);
+            this.CarbsInput.Name = "CarbsInput";
+            this.CarbsInput.Size = new System.Drawing.Size(100, 23);
+            this.CarbsInput.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(402, 114);
+            this.label4.MinimumSize = new System.Drawing.Size(0, 23);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(147, 23);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Calories from Fat :";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(383, 180);
+            this.label5.MinimumSize = new System.Drawing.Size(0, 23);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(166, 23);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Calories from Carbs :";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.SystemColors.Highlight;
+            this.label6.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label6.Location = new System.Drawing.Point(87, 308);
+            this.label6.MinimumSize = new System.Drawing.Size(0, 23);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(156, 30);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "Total Calories :";
+            // 
+            // CaloriesFromFatOutput
+            // 
+            this.CaloriesFromFatOutput.Location = new System.Drawing.Point(555, 114);
+            this.CaloriesFromFatOutput.Name = "CaloriesFromFatOutput";
+            this.CaloriesFromFatOutput.Size = new System.Drawing.Size(100, 23);
+            this.CaloriesFromFatOutput.TabIndex = 8;
+            // 
+            // CaloriesFromCarbsOutput
+            // 
+            this.CaloriesFromCarbsOutput.Location = new System.Drawing.Point(555, 180);
+            this.CaloriesFromCarbsOutput.Name = "CaloriesFromCarbsOutput";
+            this.CaloriesFromCarbsOutput.Size = new System.Drawing.Size(100, 23);
+            this.CaloriesFromCarbsOutput.TabIndex = 9;
+            // 
+            // TotslCaloriesOutput
+            // 
+            this.TotslCaloriesOutput.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.TotslCaloriesOutput.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.TotslCaloriesOutput.ForeColor = System.Drawing.SystemColors.Info;
+            this.TotslCaloriesOutput.Location = new System.Drawing.Point(249, 309);
+            this.TotslCaloriesOutput.Name = "TotslCaloriesOutput";
+            this.TotslCaloriesOutput.Size = new System.Drawing.Size(100, 29);
+            this.TotslCaloriesOutput.TabIndex = 10;
+            // 
+            // CalculateButton
+            // 
+            this.CalculateButton.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.CalculateButton.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.CalculateButton.ForeColor = System.Drawing.Color.White;
+            this.CalculateButton.Location = new System.Drawing.Point(555, 308);
+            this.CalculateButton.Name = "CalculateButton";
+            this.CalculateButton.Size = new System.Drawing.Size(100, 30);
+            this.CalculateButton.TabIndex = 11;
+            this.CalculateButton.Text = "Calculate";
+            this.CalculateButton.UseVisualStyleBackColor = false;
+            this.CalculateButton.Click += new System.EventHandler(this.CalculateButton_Click);
+            // 
+            // ExitButton
+            // 
+            this.ExitButton.BackColor = System.Drawing.Color.Red;
+            this.ExitButton.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.ExitButton.ForeColor = System.Drawing.Color.White;
+            this.ExitButton.Location = new System.Drawing.Point(449, 386);
+            this.ExitButton.Name = "ExitButton";
+            this.ExitButton.Size = new System.Drawing.Size(100, 30);
+            this.ExitButton.TabIndex = 12;
+            this.ExitButton.Text = "Exit";
+            this.ExitButton.UseVisualStyleBackColor = false;
+            this.ExitButton.Click += new System.EventHandler(this.ExitButton_Click);
+            // 
+            // ResetButton
+            // 
+            this.ResetButton.BackColor = System.Drawing.Color.Indigo;
+            this.ResetButton.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.ResetButton.ForeColor = System.Drawing.Color.White;
+            this.ResetButton.Location = new System.Drawing.Point(249, 386);
+            this.ResetButton.Name = "ResetButton";
+            this.ResetButton.Size = new System.Drawing.Size(100, 30);
+            this.ResetButton.TabIndex = 13;
+            this.ResetButton.Text = "Reset";
+            this.ResetButton.UseVisualStyleBackColor = false;
+            this.ResetButton.Click += new System.EventHandler(this.ResetButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Exercise8.Properties.Resources.lettuce;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ResetButton);
+            this.Controls.Add(this.ExitButton);
+            this.Controls.Add(this.CalculateButton);
+            this.Controls.Add(this.TotslCaloriesOutput);
+            this.Controls.Add(this.CaloriesFromCarbsOutput);
+            this.Controls.Add(this.CaloriesFromFatOutput);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.CarbsInput);
+            this.Controls.Add(this.FatGramsInput);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Calorie Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox FatGramsInput;
+        private System.Windows.Forms.TextBox CarbsInput;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox CaloriesFromFatOutput;
+        private System.Windows.Forms.TextBox CaloriesFromCarbsOutput;
+        private System.Windows.Forms.TextBox TotslCaloriesOutput;
+        private System.Windows.Forms.Button CalculateButton;
+        private System.Windows.Forms.Button ExitButton;
+        private System.Windows.Forms.Button ResetButton;
+    }
+}
+
---
+++ Exercise8/Exercise8/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+/* * * * * * * * * * * * *
+ * Warren Peterson * * * *
+ * This is my own work * *
+ * 4/21/2021 CST-117 * * *
+ * Exercise 8  * * * * * *
+ * Calorie Calculator App*
+ * * * * * * * * * * * * *
+ * * * * * * * * * * * * */
+namespace Exercise8
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        /* * This is the Calculate Calories Button * * * * * * * * */
+        private void CalculateButton_Click(object sender, EventArgs e)
+        {
+            // Variable Declarations
+            int carbCalories = CarbCalories(); // Declares the variable carbCalories will be set to the result from the CarbCalorie Method
+            int fatCalories = FatCalories(); // This variable will be used to hold the result from the FatCalories method
+            int sum = carbCalories + fatCalories; // This variable will be used to hold the sum of the results from both methods
+            CaloriesFromCarbsOutput.Text = carbCalories.ToString(); //  Outputs result
+            CaloriesFromFatOutput.Text = fatCalories.ToString(); // Outputs results
+            TotslCaloriesOutput.Text = sum.ToString(); // Displays Result
+
+        }
+        // The CarbCalories Method
+        private int CarbCalories()
+        {
+            // Variables
+            int carbs; // This variable will hold the carb gram input integer
+            bool result = Int32.TryParse(CarbsInput.Text, out carbs); // Creates variable to hold result
+            // This loop will return true if there is a result and will return false(0) if there is no result and display an error
+            if (result)
+            {
+                return carbs * 4;
+            }
+            else
+            {
+                MessageBox.Show("Invalid Input, Please enter a number");
+                return 0;
+            }
+        }
+        // The Fat Calories Method
+        private int FatCalories()
+        {
+            // Variables
+            int fat; // Variable to hold the fat grams input integer
+            bool result = Int32.TryParse(FatGramsInput.Text, out fat); // Variable to hold result
+            // This loop will return true if there is a result and will return false(0) if there is no result and display an error
+            if (result)
+            {
+                return fat * 9;
+            }
+            else
+            {
+                MessageBox.Show("Invalid Entry, Please Enter a real Number Sir");
+                return 0;
+            }
+        }
+        // Exit Program Method
+        private void ExitProgram()
+        {
+            this.Close();
+        }
+        // Clear Fields Method
+        private void ResetProgram()
+        {
+            CarbsInput.Text = "";
+            CaloriesFromCarbsOutput.Text = "";
+            FatGramsInput.Text = "";
+            CaloriesFromFatOutput.Text = "";
+            TotslCaloriesOutput.Text = "";
+        }
+        // Exit button invokes Exit Program method
+        private void ExitButton_Click(object sender, EventArgs e)
+        {
+            ExitProgram();
+        }
+        // Reset Button invokes Clear Fields method
+        private void ResetButton_Click(object sender, EventArgs e)
+        {
+            ResetProgram();
+        }
+    }
+}
