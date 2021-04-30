9dab603aeb35ea77bc4f61b22619fea084080e34
WarrenGSlice <77131351+WarrenGSlice@users.noreply.github.com>
2021-04-30 15:10:01 -0700

    Add files via upload

---
+++ Exercise9/Exercise9/Program.cs
@@ -0,0 +1,53 @@
+﻿//use for IC08
+//Lydia's code
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* * * * * * * * * * * *
+ * Warren Peterson * * *
+ * This is my own work *
+ * CST-117 4/30/2021 * *
+ * Exercise 9  * * * * *
+ * Code Checking * * * *
+ * * * * * * * * * * * */
+
+// Corrected by <Warren Peterson>
+
+namespace Exercise9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //make some sets
+            Set A = new Set();
+            Set B = new Set();
+
+            //put some stuff in the sets
+            Random r = new Random();
+            for (int i = 0; i < 10; i++)
+            {
+                //Due to checking against duplicates in Set, there should never be more than four elements, we want to iterate through this ten times.
+ //original     A.addElement(r.Next(4));
+
+                //Updated to 10 to get a max of ten elements.
+                A.addElement(r.Next(10));
+                B.addElement(r.Next(12));
+            }
+
+            //display each set and the union
+            Console.WriteLine("A: " + A);
+            Console.WriteLine("B: " + B);
+            Console.WriteLine("A union B: " + A.union(B));
+
+            //display original sets (should be unchanged)
+            Console.WriteLine("After union operation");
+            Console.WriteLine("A: " + A);
+            Console.WriteLine("B: " + B);
+            Console.ReadKey();  // Added This method keep the console from closing on its own
+        }
+    }
+}
---
+++ Exercise9/Exercise9/Set.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Lydia's code - find the errors!
+/* * * * * * * * * * * *
+ * Warren Peterson * * *
+ * This is my own work *
+ * CST-117 4/30/2021 * *
+ * Exercise 9  * * * * *
+ * Code Checking * * * *
+ * * * * * * * * * * * */
+
+// Corrected by <Warren Peterson>
+
+namespace Exercise9
+{
+    class Set
+    {
+        private List<int> elements;
+
+        public Set()
+        {
+            elements = new List<int>();
+        }
+        public bool addElement(int val)
+        {
+            if (ContainsElement(val)) return false;
+            else
+            {
+                elements.Add(val);
+                return true;
+            }
+        }
+        private bool ContainsElement(int val)
+        {
+            return elements.Contains(val);  //corrected code line
+            //replaced with List<T>.Contains method
+
+            //for (int i = 0; i < elements.Count; i++)
+            //{
+            //    if (val == elements[i])
+            //        return true;
+            //    else
+            //        return false;
+            //}
+            //return false;
+        }
+        public override string ToString()
+        {
+            string str = "";
+            foreach (int i in elements)
+            {
+                str += i + " ";
+            }
+            return str;
+        }
+        public void clearSet()
+        {
+            elements.Clear();
+        }
+        public Set union(Set rhs)
+        {
+            // needs to return a new Set object instead of adding to current object.
+            Set union = new Set();
+
+            //Add the current elements to the new object.
+            for (int i = 0; i < this.elements.Count; i++)
+            {
+                union.addElement(this.elements[i]);
+            }
+            for (int i = 0; i < rhs.elements.Count; i++)
+            {
+                //Adds elements to the current object
+                //this.addElement(rhs.elements[i]);
+                union.addElement(rhs.elements[i]);
+            }
+            return union;
+            //return rhs;
+        }
+    }
+}
